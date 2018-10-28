15aadcaf276c6b233c7ef73ad60fa65d4d3efbba
Stan <stan.chmielewski@gmail.com>
2018-10-28 20:52:26 +0100

    1 ST commit

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
 
-	fmt.Println(fibonacci(0, 2))
+	fmt.Println(fibonacci(0, 2, 0))
+
+	//hgjgfg
 	/*
 		nextOdd := makeOddGenerator()
 		fmt.Println(nextOdd()) // 1
@@ -31,68 +33,70 @@ func fibonacci(x int, y int, z int) func() int {
 		z, x, y = x, y, x+y
 		return z
 	}
-// }
+	// }
 
-/*
-The Fibonacci sequence is defined as:
-fib(0) = 0, fib(1) = 1, fib(n) = fib(n-1) + fib(n-2).
-Write a recursive function which can find fib(n).
-*/
-func fibonacci(startNum int, addCount int) int {
-	currNum := startNum
-	nextNum := currNum + 1
-	result := 0
-	for i := 0; i <= addCount; i++ {
-		result = func(currNum int, nextNum int) int {
-			return currNum + nextNum
-		}
-		currNum = nextNum
-		nextNum = result
-	}
-	fmt.Println(result)
-	return result
-}
+	/*
+	   The Fibonacci sequence is defined as:
+	   fib(0) = 0, fib(1) = 1, fib(n) = fib(n-1) + fib(n-2).
+	   Write a recursive function which can find fib(n).
 
-func avarage(xs []float64) float64 {
-	total := 0.0
-	for _, v := range xs {
-		total += float64(v)
-	}
-	return total / float64(len(xs))
-}
+	   func fibonacci(startNum int, addCount int) int {
+	   	currNum := startNum
+	   	nextNum := currNum + 1
+	   	result := 0
+	   	for i := 0; i <= addCount; i++ {
+	   		result = func(currNum int, nextNum int) int {
+	   			return currNum + nextNum
+	   		}
+	   		currNum = nextNum
+	   		nextNum = result
+	   	}
+	   	fmt.Println(result)
+	   	return result
+	   }
 
-func half(x int) (result int, isEven bool) {
-	result = x / 2
-	if x%2 == 0 {
-		isEven = true
-	}
-	return result, isEven
-}
+	   func avarage(xs []float64) float64 {
+	   	total := 0.0
+	   	for _, v := range xs {
+	   		total += float64(v)
+	   	}
+	   	return total / float64(len(xs))
+	   }
 
-func greatest(numbers ...int) int {
-	theBest := 0
-	for _, n := range numbers {
-		if theBest < n {
-			theBest = n
-		}
-	}
-	return theBest
-}
+	   func half(x int) (result int, isEven bool) {
+	   	result = x / 2
+	   	if x%2 == 0 {
+	   		isEven = true
+	   	}
+	   	return result, isEven
+	   }
 
-func makeEvenGenerator() func() uint {
-	i := uint(0)
-	return func() (ret uint) {
-		ret = i
-		i += 2
-		return
-	}
-}
+	   func greatest(numbers ...int) int {
+	   	theBest := 0
+	   	for _, n := range numbers {
+	   		if theBest < n {
+	   			theBest = n
+	   		}
+	   	}
+	   	return theBest
+	   }
 
-func makeOddGenerator() func() uint {
-	i := uint(1)
-	return func() (ret uint) {
-		ret = i
-		i += 2
-		return
-	}
+	   func makeEvenGenerator() func() uint {
+	   	i := uint(0)
+	   	return func() (ret uint) {
+	   		ret = i
+	   		i += 2
+	   		return
+	   	}
+	   }
+
+	   func makeOddGenerator() func() uint {
+	   	i := uint(1)
+	   	return func() (ret uint) {
+	   		ret = i
+	   		i += 2
+	   		return
+	   	}
+	   }
+	*/
 }
